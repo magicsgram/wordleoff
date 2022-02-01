bb0540db32dd676103d942af9b81ab0ed746441a
Kyle Koh <kyle@zeusliving.com>
2022-02-01 11:53:54 -0800

    remove manual publish code

--- publish.bat
+++
@@ -1,4 +0,0 @@
-wsl rm single_file.tar.xz
-start /B /low /wait dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
-wsl XZ_OPT=-9 tar -Jcvf single_file.tar.xz webpublish/
-wsl rm -rf webpublish/
--- publish.sh
+++
@@ -1,3 +0,0 @@
-rm -rf webpublish/
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
-
