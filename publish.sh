27cf47485876f19f20ead60b441ab9179bd477bc
Kyle Koh <kyle@zeusliving.com>
2022-02-01 03:19:08 -0800

    dockerfile

--- publish.sh
+++ publish.sh
@@ -1,4 +1,3 @@
-rm single_file.tar.xz
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
-XZ_OPT=-9 tar -Jcvf single_file.tar.xz webpublish/
 rm -rf webpublish/
+dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
+
