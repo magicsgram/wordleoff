7cbd392f2b72ebaf72a0ba32c48b233cad086c43
Kyle <magicsgram@outlook.com>
2022-02-04 04:05:06 -0800

    optimizations

---
+++ Client/BrowserResizeService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace WordleOff.Client;
+
+public class BrowserResizeService
+{
+  public static event Func<Task>? OnResize;
+
+  [JSInvokable]
+  public static async Task OnBrowserResize()
+  {
+    if (OnResize is not null)
+      await OnResize.Invoke();
+  }  
+}
---
+++ Client/wwwroot/js/browserResize.js
@@ -0,0 +1,14 @@
+﻿window.browserResize = {
+  getInnerHeight: function () {
+    return window.innerHeight;
+  },
+  getInnerWidth: function () {
+    return window.innerWidth;
+  },
+  registerResizeCallback: function () {
+    window.addEventListener("resize", browserResize.resized);
+  },
+  resized: function () {
+    DotNet.invokeMethodAsync("WordleOff.Client", "OnBrowserResize").then(data => data);
+  }
+}
