27e821471c0cc783fe7d20e354c5ed9df8a98762
Kyle <magicsgram@outlook.com>
2022-02-16 23:29:05 -0800

    optimize animation

---
+++ Client/wwwroot/js/reanimateColored.js
@@ -0,0 +1,10 @@
+﻿window.reanimateAll = {
+  reanimateAll: function () {
+    document.querySelectorAll('.colored').forEach(function (tile) {
+      tile.style.animation = "none";
+      setTimeout(function () {
+        tile.style.animation = '';
+      }, 1);
+    });
+  }
+}
